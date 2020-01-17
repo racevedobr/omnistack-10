fba1125748f400dca14a0300aa4a2810c1480069
QA DevSoft <rodrigoacevedo@outlook.com>
2020-01-16 23:53:15 -0300

    Terminado app dos celular e app funcionando corretamente

--- mobile/mobile/src/routes.js
+++ mobile/mobile/src/routes.js
@@ -11,7 +11,7 @@ const Routes = createAppContainer(
         Main: {
             screen: Main,
             navigationOptions: {
-            title: 'DevRadar',     
+            title: 'DevRadar',
             },
         },
         Profile: {
