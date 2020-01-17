441756abbcec7f4523f056847d3e1d5bb8752f45
Rodrigo Acevedo <rodrigo.acevedo@wasys.combr>
2020-01-17 15:00:22 -0300

    Realizado ajustes no badk e no mobile, criado novas funções e filtros

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const mongooose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
 const routes = require('./routes');
+const { setubWebsocket } = require('./websocket')
 
 const app = express();
+const server = http.Server(app);
+
+setubWebsocket(server);
 
 mongooose.connect('mongodb+srv://projetos:projetos2020@projetos-avdnj.mongodb.net/week10?retryWrites=true&w=majority',{
     useNewUrlParser: true,
@@ -15,4 +20,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333)
+server.listen(3333);
